7387560b2f443b19775a15816de876927c6bfd2e
Emre Can Seçer <103491438+seceremrecan@users.noreply.github.com>
2023-06-20 18:55:09 +0300

    without-docker

--- app.py
+++ app.py
@@ -1,59 +1,132 @@
-from flask import Flask, jsonify
-
-
+from flask import Flask, jsonify, render_template, redirect, url_for, request
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from apscheduler.schedulers.background import BackgroundScheduler
 from praw import Reddit
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from models import Base, Post
+from models import Base, Post, User
+import configparser
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
+# Base.metadata.drop_all(engine)
+# Base.metadata.create_all(engine)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-CLIENT_ID = 'HnuSMFN7tsJ1z8y4SUAxVw'
-CLIENT_SECRET = 'ycQCCc0VndHVPpMDv9EYu95449Msrg'
-USERNAME = 'seceremre'
-PASSWORD = '258258258eE.'
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 # Reddit API'ye bağlanma
-reddit = Reddit(client_id=CLIENT_ID,
-                client_secret=CLIENT_SECRET,
-                username=USERNAME,
-                password=PASSWORD,
+reddit = Reddit(client_id=config['reddit']['client_id'],
+                client_secret=config['reddit']['client_secret'],
+                username=config['reddit']['username'],
+                password=config['reddit']['password'],
                 user_agent='defanceProject')
 
 # PostgreSQL veritabanı bağlantısı oluşturma
-DATABASE_URL = 'postgresql://postgres:258258258@localhost:5432/brandefance'
+DATABASE_URL = config['database']['url']
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return session.query(User).get(user_id)
+
 @app.route('/', methods=['GET'])
 def home():
     return 'Welcome to Reddit Crawler API'
-# Tüm postları döndüren endpoint
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = session.query(User).filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+
+        return 'Invalid username or password'
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = session.query(User).filter_by(username=username).first()
+        if existing_user:
+            return 'Bu kullanıcı adı zaten kullanılıyor. Lütfen farklı bir kullanıcı adı deneyin.'
+
+        new_user = User(username=username, password=password)
+        session.add(new_user)
+        session.commit()
+
+        return 'Kayıt işlemi başarılı. Artık giriş yapabilirsiniz.'
+
+    return render_template('register.html')
+
 @app.route('/posts', methods=['GET'])
+@login_required
 def get_posts():
     posts = session.query(Post).all()
     result = [{'id': post.id, 'title': post.title, 'subreddit': post.subreddit} for post in posts]
     return jsonify(result)
 
-# Reddit API'ye istek atarak en son postları çeken endpoint
 @app.route('/crawl', methods=['GET'])
+@login_required
 def crawl_posts():
-    subreddit_name = 'python'  # İstediğiniz subreddit'in adını buraya yazın
-    subreddit = reddit.subreddit(subreddit_name)
-    posts = subreddit.new(limit=10)  # En son 10 postu çekin
+    try:
+        subreddit_name = 'python'
+        subreddit = reddit.subreddit(subreddit_name)
+        posts = subreddit.new(limit=10)
 
-    for post in posts:
-        new_post = Post(title=post.title, subreddit=subreddit_name)
-        session.add(new_post)
+        for post in posts:
+            if session.query(Post).get(post.id) is None:
+                new_post = Post(id=post.id, title=post.title, subreddit=subreddit_name)
+                session.add(new_post)
 
-    session.commit()
-    return 'Posts crawled and saved successfully'
+        session.commit()
+        return 'Posts crawled and saved successfully'
+    except Exception as e:
+        session.rollback()
+        return str(e), 500
+
+def fetch_posts():
+    try:
+        subreddit_name = 'python'
+        subreddit = reddit.subreddit(subreddit_name)
+        posts = subreddit.new(limit=10)
+
+        for post in posts:
+            new_post = Post(id=post.id, title=post.title, subreddit=subreddit_name)
+            session.add(new_post)
+
+        session.commit()
+        print('Posts crawled and saved successfully')
+    except Exception as e:
+        session.rollback()
+        print(f"Error while fetching posts: {str(e)}")
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(fetch_posts, 'interval', minutes=1)
+scheduler.start()
 
 if __name__ == '__main__':
-    
+    Base.metadata.create_all(engine)
     app.run(debug=True)
-Base.metadata.create_all(engine)    
--- models.py
+++ models.py
@@ -1,13 +1,44 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import validates
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 Base = declarative_base()
 
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password_hash = Column(String(100), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError('Username is required.')
+        return username
+
+
+
 class Post(Base):
     __tablename__ = 'posts'
+
     id = Column(Integer, primary_key=True)
     title = Column(String)
     subreddit = Column(String)
 
-    def __repr__(self):
-        return f"<Post(id={self.id}, title='{self.title}', subreddit='{self.subreddit}')>"
+    def __init__(self, id, title, subreddit):
+        self.id = id
+        self.title = title
+        self.subreddit = subreddit
